df1ae52e5c2d656c0a1d89ba52871396330e5b9a
VedantKashettiwar <kvedant164@gmail.com>
2022-11-24 18:34:03 +0530

    first commit

--- controllers/personal.js
+++ controllers/personal.js
@@ -14,33 +14,33 @@ const chunk = require("chunk");
 //                 // You can use initializeUnorderedBulkOp to initialize Unordered Batch
 //                 var bulk = col.initializeOrderedBulkOp();
 
-                // for (var i = 0; i < 2000; ++i) {
-                //   bulk.insert({
-                //     name: "Vedant",
-                //     email: "kvedant164@gmail.com",
-                //     phone: 7020576985,
-                //   });
-                //   bulk.insert({
-                //     name: "Tanmay",
-                //     email: "tanmay@gmail.com",
-                //     phone: 7020576985,
-                //   });
-                //   bulk.insert({
-                //     name: "Akshata",
-                //     email: "akshata@gmail.com",
-                //     phone: 7020576985,
-                //   });
-                //   bulk.insert({
-                //     name: "Saurabh",
-                //     email: "saurabh@gmail.com",
-                //     phone: 7020576985,
-                //   }),
-                //     bulk.insert({
-                //       name: "Rahul",
-                //       email: "rahul@gmail.com",
-                //       phone: 7020576985,
-                //     });
-                // }
+// for (var i = 0; i < 2000; ++i) {
+//   bulk.insert({
+//     name: "Vedant",
+//     email: "kvedant164@gmail.com",
+//     phone: 7020576985,
+//   });
+//   bulk.insert({
+//     name: "Tanmay",
+//     email: "tanmay@gmail.com",
+//     phone: 7020576985,
+//   });
+//   bulk.insert({
+//     name: "Akshata",
+//     email: "akshata@gmail.com",
+//     phone: 7020576985,
+//   });
+//   bulk.insert({
+//     name: "Saurabh",
+//     email: "saurabh@gmail.com",
+//     phone: 7020576985,
+//   }),
+//     bulk.insert({
+//       name: "Rahul",
+//       email: "rahul@gmail.com",
+//       phone: 7020576985,
+//     });
+// }
 
 //                 // Execute the operations
 //                 bulk.execute(function (err, result) {
@@ -59,54 +59,54 @@ const chunk = require("chunk");
 const createDetails = async (req, res) => {
   try {
     for (var i = 0; i < 2000; ++i) {
-    Personals.bulkWrite([
-      {
-        insertOne: {
-          document: {
-            name: "Vedant",
-            email: "kvedant164@gmail.com",
-            phone: 7020576985,
+      Personals.bulkWrite([
+        {
+          insertOne: {
+            document: {
+              name: "Vedant",
+              email: "kvedant164@gmail.com",
+              phone: 7020576985,
+            },
           },
         },
-      },
-      {
-        insertOne: {
-          document: {
-            name: "Tanmay",
-            email: "tanmay@gmail.com",
-            phone: 7020576985,
+        {
+          insertOne: {
+            document: {
+              name: "Tanmay",
+              email: "tanmay@gmail.com",
+              phone: 7020576985,
+            },
           },
         },
-      },
-      {
-        insertOne: {
-          document: {
-            name: "Akshata",
-            email: "akshata@gmail.com",
-            phone: 7020576985,
+        {
+          insertOne: {
+            document: {
+              name: "Akshata",
+              email: "akshata@gmail.com",
+              phone: 7020576985,
+            },
           },
         },
-      },
-      {
-        insertOne: {
-          document: {
-            name: "Saurabh",
-            email: "saurabh@gmail.com",
-            phone: 7020576985,
+        {
+          insertOne: {
+            document: {
+              name: "Saurabh",
+              email: "saurabh@gmail.com",
+              phone: 7020576985,
+            },
           },
         },
-      },
-      {
-        insertOne: {
-          document: {
-            name: "Rahul",
-            email: "rahul@gmail.com",
-            phone: 7020576985,
+        {
+          insertOne: {
+            document: {
+              name: "Rahul",
+              email: "rahul@gmail.com",
+              phone: 7020576985,
+            },
           },
         },
-      },
-    ])
-  }
+      ]);
+    }
     res.status(200).json("Added Successfully");
   } catch (err) {
     res.status(500).json(err);
@@ -121,14 +121,16 @@ const showDetails = async (req, res) => {
     res.status(500).json(err);
   }
 };
-const savePersonal = (item)=>{
+
+const savePersonal = (item) => {
   const add = {
-    name:item.name,
-    email:item.email,
-    phone:item.phone
-  }
-  return {insertOne:{"document":add}}
-}
+    name: item.name,
+    email: item.email,
+    phone: item.phone,
+  };
+  return { insertOne: { document: add } };
+};
+
 const fetchDetails = async (req, res) => {
   try {
     const arrayDetails = await Personals.find();
@@ -137,9 +139,9 @@ const fetchDetails = async (req, res) => {
       if (index === data.length) {
         return res.status(200).send("Fetch And Saved");
       } else {
-        const array = data[index]
-        const newArray = array.map(savePersonal)
-        FetchPersonals.bulkWrite(newArray)
+        const array = data[index];
+        const newArray = array.map(savePersonal);
+        FetchPersonals.bulkWrite(newArray);
         saveData(index + 1, data, res);
       }
     }
@@ -149,13 +151,85 @@ const fetchDetails = async (req, res) => {
   }
 };
 
-const saveDetailsByPagination = async(req,res)=>{
-  try{
-    res.status(200).json("Saved")
+const saveDetailsByPagination = async (req, res) => {
+  try {
+    const arrayDetails = await Personals.find();
+    const data = chunk(arrayDetails, 50);
+    function saveData(index, data, res) {
+      if (index === data.length) {
+        return res.status(200).send("Fetch And Saved");
+      } else {
+        let array = data[index];
+        function saveArraydata(indexMain, array, start_, end_, limit) {
+          let al = array.length / limit;
+          al = Math.ceil(al);
+          if (indexMain !== al) {
+            let start = start_;
+            let end = end_;
+            let array1 = array.slice(start, end);
+            // console.log(array1)
+            const newArray = array1.map(savePersonal);
+            FetchPersonals.bulkWrite(newArray);
+            start = start + limit;
+            end = end + limit;
+            saveArraydata(indexMain + 1, array, start, end, limit);
+          } else {
+            return false;
+          }
+        }
+        let limit = 2;
+        saveArraydata(0, array, 0, limit, limit);
+        saveData(index + 1, data, res);
+      }
+    }
+    saveData(0, data, res);
+  } catch (err) {
+    res.status(500).send("Error");
   }
-  catch(err){
-    res.status(500).json(err)
-  }
-} 
+};
 
-module.exports = { createDetails, fetchDetails, showDetails,saveDetailsByPagination };
+const pagination =async (page, limit)=>{
+  const data = await Personals.find().skip((page - 1) * limit).limit(limit);
+  if(data.length == 0){
+    return {
+      next: false,
+      page: page,
+      limit: limit,
+      data: data
+    }
+  }
+  else{
+    return {
+      next: true,
+      page: page,
+      limit: limit,
+      data: data
+    }
+  }
+}
+
+const saveDetailsByPagination2 = async (req, res) => {
+  try {
+    const saveArraydata = async (page, limit) => {
+      const pageData = await pagination(page,limit)
+      if (pageData.next) {
+        const newArray = pageData.data.map(savePersonal);
+        FetchPersonals.bulkWrite(newArray);
+        saveArraydata(pageData.page + 1, pageData.limit);
+      } else {
+        res.status(200).json("Fetch and Save");
+      }
+    };
+    saveArraydata(1, 10);
+  } catch (err) {
+    res.status(500).send("Error");
+  }
+};
+
+module.exports = {
+  createDetails,
+  fetchDetails,
+  showDetails,
+  saveDetailsByPagination,
+  saveDetailsByPagination2,
+};
--- routes/personal.js
+++ routes/personal.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router()
 
-const{createDetails,fetchDetails,showDetails,saveDetailsByPagination} = require('../controllers/personal')
+const{createDetails,fetchDetails,showDetails,saveDetailsByPagination,saveDetailsByPagination2} = require('../controllers/personal')
     
 
 router.route('/createpersonal/').post(createDetails)
 router.route('/fetchdetails/').get(fetchDetails)
 router.route('/showdetails/').get(showDetails)
 router.route('/savedetailsbypagination/').get(saveDetailsByPagination)
+router.route('/savedetailsbypagination2/').get(saveDetailsByPagination2)
 
 module.exports = router
